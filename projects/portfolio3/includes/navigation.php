561e572c130205d6b360b97f3664ea601ae77a36
KuebikoYT <kuebikovideos@gmail.com>
2018-02-10 21:28:11 -0700

    portfolio

---
+++ projects/portfolio3/includes/navigation.php
@@ -0,0 +1,32 @@
+
+
+
+<style>
+li {
+  display: inline-block;
+}
+
+.nav {
+  text-align: right;
+  padding-right: 2rem;
+}
+
+.navitem {
+  text-decoration: none;
+  color: black;
+  font-family: arial;
+  padding-left: .25rem;
+  padding-right: .25rem;
+}
+
+
+</style>
+
+
+    <div class="nav">
+    <ul>
+      <li><a href="" class="navitem">Home</a></li>
+      <li><a href="" class="navitem">Portfolio</a></li>
+      <li><a href="" class="navitem">Contact</a></li>
+    </ul>
+  </div>
